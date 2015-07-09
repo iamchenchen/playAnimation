99875f3784386435fd739bd978ba0b15b99f8956
Chenchen Zheng <chenchen.zheng@fireeye.com>
2015-07-09 10:56:21 -0700

    add basic file

---
+++ animation/animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  animation
+//
+//  Created by Chenchen Zheng on 7/9/15.
+//  Copyright (c) 2015 Chenchen Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
